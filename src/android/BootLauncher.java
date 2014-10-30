11ff410a9b24a054d3c2bcea9666f8559ec13284
Lee Crossley <leee@hotmail.co.uk>
2014-10-30 15:14:16 +0000

    Change ns

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -1,4 +1,4 @@
-package uk.co.ilee.bootlauncher;
+package uk.co.payzone.bootlauncher;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -10,7 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, uk.co.payzone.app.class);
+        Intent serviceIntent = new Intent(context, uk.co.payzone.app.CordovaApp);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
