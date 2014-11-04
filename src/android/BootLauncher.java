6bbadc3f8d66f2fa70c30baefc5235b526cbb9d6
Lee Crossley <leee@hotmail.co.uk>
2014-11-04 11:11:54 +0000

    Update package name

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -1,4 +1,4 @@
-package uk.co.payzone.bootlauncher;
+package uk.co.ilee.bootlauncher;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -10,7 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, uk.co.payzone.app.CordovaApp);
+        Intent serviceIntent = new Intent(context, uk.co.ilee.CordovaApp.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
